f44d57b0771673feb588eef67cc1d4266a93d21a
Emilia <emilia.mena26@gmail.com>
2019-10-11 20:44:37 -0400

    adding sessions, password validations, and including active model validations in traveler class for validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-
+  
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "pineapple_express"
+    register Sinatra::Flash
+    require 'sinatra/flash'
   end
 
   get "/" do
--- app/models/traveler.rb
+++ app/models/traveler.rb
@@ -1,4 +1,11 @@
 class Traveler < ActiveRecord::Base
+    include ActiveModel::Validations
+    
     has_many :goals
     has_many :destinations, through: :goals
+
+    has_secure_password
+    validates_presence_of :name, :email, :password
+    validates :email, presence: true, format: { with: /A[^@s] @[^@s] z/, allow_blank: true }
+    #validates :email, uniqueness: true
 end 
