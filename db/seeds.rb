2bf627885cdbead998c9676ed959f6c932ae2a7c
Emilia <emilia.mena26@gmail.com>
2019-10-15 18:36:57 -0400

    cleared test db info and added seeds.rb

---
+++ db/seeds.rb
@@ -0,0 +1,24 @@
+Traveler.create(name: "Emi", email: "emi@example.com", password: "password")
+Traveler.create(name: "Alex", email: "alex@example.com", password: "password")
+Traveler.create(name: "Liz", email: "liz@example.com", password: "password")
+Traveler.create(name: "Filippe", email: "surfer@example.com", password: "password")
+
+
+Goal.create(title: "Travel to Brazil", traveler_id: 1)
+Goal.create(title: "Travel to Greece", traveler_id: 1)
+Goal.create(title: "Travel to India", traveler_id: 1)
+Goal.create(title: "Travel to Spain", traveler_id: 1)
+Goal.create(title: "Travel to Cape Verde", traveler_id: 1)
+
+Goal.create(title: "Travel to New Zealand", traveler_id: 2)
+Goal.create(title: "Travel South America", traveler_id: 2)
+
+Goal.create(title: "Travel to Brazil", traveler_id: 3)
+Goal.create(title: "Travel to Holland", traveler_id: 3)
+Goal.create(title: "Travel to New York", traveler_id: 3)
+Goal.create(title: "Travel to Chicago", traveler_id: 3)
+
+Goal.create(title: "Travel to California", traveler_id: 4)
+Goal.create(title: "Travel to Chile", traveler_id: 4)
+Goal.create(title: "Travel to Argentina", traveler_id: 4)
+Goal.create(title: "Travel to Best Surfer Spots", traveler_id: 4)
