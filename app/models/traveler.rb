16c53c293dd754b3ff7989566bd053c8dc99deca
Emilia <emilia.mena26@gmail.com>
2019-10-15 07:53:22 -0400

    traveler was not creating, tested few options, changed the validation in Traveler class and now account is creating

--- app/models/traveler.rb
+++ app/models/traveler.rb
@@ -6,6 +6,7 @@ class Traveler < ActiveRecord::Base
 
     has_secure_password
     validates_presence_of :name, :password
-    validates :email, presence: true, format: { with: /A[^@s] @[^@s] z/, allow_blank: true }
+    #validates :email, presence: true, format: { with: /A[^@s] @[^@s] z/, allow_blank: true }
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
     validates :email, uniqueness: true
 end 
